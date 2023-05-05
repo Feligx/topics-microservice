a1baf6c07a4dc9d88b67049244fca9bd30cce6b5
Feligx <feligxgamer@gmail.com>
2023-05-04 19:37:39 -0500

    Sanitizing inputs

--- routes/topicsRouter.js
+++ routes/topicsRouter.js
@@ -1,69 +1,94 @@
 const express = require('express')
+const { body, param, validationResult, query} = require('express-validator')
 const topicsService = require('../services/topicsService')
-
 const router = express.Router()
 const service = new topicsService()
 
-router.get('/', async (req, res, next) => {
+router.get('/', [query('name').optional().isString().escape(), query('limit').optional().isInt({min: 0}).escape(), query('limit').optional().isInt({min: 0}).escape()], async (req, res, next) => {
+    const validationErrors = validationResult(req)
+    if (validationErrors.isEmpty()) {
 
-    try {
-        const topics = await service.getTopics(req.query || {} )
+        try {
+            const topics = await service.getTopics(req.query || {})
 
-        res.json({
-            topics
-        })
-    } catch (error) {
-        next(error)
+            res.json({
+                topics
+            })
+        } catch (error) {
+            next(error)
+        }
+    } else {
+        next(new Error(JSON.stringify({message: 'Invalid query params', status: 400})))
     }
 })
 
-router.get('/:id', async (req, res, next) => {
-    console.log(`Get topic ${req.params.id}`)
-    try {
-        const { id } = req.params
-        const topic = await  service.getTopic(id)
-        return res.json(topic)
-    } catch (error) {
-        next(error)
+router.get('/:id', param('id').exists().isString().isMongoId().escape() , async (req, res, next) => {
+    const validationErrors = validationResult(req)
+    if (validationErrors.isEmpty()) {
+        try {
+            const { id } = req.params
+            const topic = await  service.getTopic(id)
+            return res.json(topic)
+        } catch (error) {
+            next(error)
+        }
+    } else {
+        next(new Error(JSON.stringify({message: 'Invalid id', status: 400})))
     }
 })
 
-router.put('/:id', async (req, res, next) => {
-
-    try {
-        const { id } = req.params
-        res.json(await service.updateTopic(id, req.body))
-    } catch (error) {
-        next(error)
+router.put('/:id', param('id').exists().isString().isMongoId().escape(), async (req, res, next) => {
+    const validationErrors = validationResult(req)
+    if (validationErrors.isEmpty()) {
+        try {
+            const {id} = req.params
+            res.json(await service.updateTopic(id, req.body))
+        } catch (error) {
+            next(error)
+        }
+    } else {
+        next(new Error(JSON.stringify({message: 'Invalid id', status: 400})))
     }
 })
 
-router.post('/', async (req, res, next) => {
-    try {
-        const { name } = req.body
-        await service.createTopic(name)
+router.post('/', body('name').notEmpty().exists().isString().escape(),  async (req, res, next) => {
 
-        res.status(201).json({
-            message: 'Topic created'
-        })
-    } catch (error) {
-        next(error)
+    const validationErrors = validationResult(req)
+
+    if (validationErrors.isEmpty()) {
+        try {
+            const {name} = req.body
+            await service.createTopic(name)
+
+            res.status(201).json({
+                message: 'Topic created'
+            })
+        } catch (error) {
+            next(error)
+        }
+    } else {
+        next(new Error(JSON.stringify({message: 'Invalid name', status: 400})))
     }
 })
 
-router.delete('/:id', async (req, res, next) => {
-    console.log(req.params.id)
+router.delete('/:id', param('id').exists().isString().isMongoId().escape(), async (req, res, next) => {
+
+    const validationErrors = validationResult(req)
 
     const { id } = req.params
 
-    try {
-        await service.deleteTopic(id)
+    if (validationErrors.isEmpty()) {
+        try {
+            await service.deleteTopic(id)
 
-        res.json({
-            message: 'Topic deleted'
-        })
-    } catch (error) {
-        next(error)
+            res.json({
+                message: 'Topic deleted'
+            })
+        } catch (error) {
+            next(error)
+        }
+    } else {
+        next(new Error(JSON.stringify({message: 'Invalid id', status: 400})))
     }
 })
 
